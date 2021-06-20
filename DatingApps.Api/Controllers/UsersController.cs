fd2b090de893d0385a24f213c836ffc6bb86d1c5
herohl <hero.handoko@gmail.com>
2021-06-20 12:47:15 +0700

    first commit

---
+++ DatingApps.Api/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using DatingApps.Api.Data;
+using DatingApps.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApps.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public UsersController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await this.context.Users.ToListAsync();
+        }
+
+        // api/users/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await this.context.Users.FindAsync(id);
+        }
+    }
+}
