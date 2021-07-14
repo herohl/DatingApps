284f38da8f57b4ff350f35548d98f64692772aa6
herohl <hero.handoko@gmail.com>
2021-07-14 17:27:55 +0700

    from manjaro

--- DatingApps.Api/Entities/AppUser.cs
+++ DatingApps.Api/Entities/AppUser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApps.Api.Extensions;
 
 namespace DatingApps.Api.Entities
 {
@@ -11,5 +12,20 @@ namespace DatingApps.Api.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
